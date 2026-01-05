7edf959b18d5edbc12bc3d5395b11642c918986d
Addison Vincelli <addison.vincelli@gmail.com>
2026-01-05 12:52:13 -0500

    added login.js

---
+++ js/logIn.js
@@ -0,0 +1,86 @@
+ // ----------------- User Log-In Page --------------------------------------//
+
+ // ----------------- Firebase Setup & Initialization ------------------------//
+
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+
+ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } 
+ from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+
+import {getDatabase, ref, set, update, child, get}
+ from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
+
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyCptQUzhXL4nLiRxvEuhuINLKRRfuKK4zY",
+   authDomain: "sci-fi-showcase.firebaseapp.com",
+   databaseURL: "https://sci-fi-showcase-default-rtdb.firebaseio.com",
+   projectId: "sci-fi-showcase",
+   storageBucket: "sci-fi-showcase.firebasestorage.app",
+   messagingSenderId: "794618128894",
+   appId: "1:794618128894:web:216c3d0b1fee81e4b538af"
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+
+ //Initialize Firebase Authentication
+const auth = getAuth();
+
+//Return instance of your app's Firebase Realtime Database (FRD)
+const db = getDatabase(app);
+
+// ---------------------- Sign-In User ---------------------------------------//
+document.getElementById('signIn').onclick = function(){
+
+    //Get user's email and password for signing in
+    const email = document.getElementById('loginEmail').value;
+    const password = document.getElementById('loginPassword').value;
+    console.log(email, password);
+
+    //Attempt to sign in the user
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential)=>{
+        //Create user credential and store the user ID
+        const user = userCredential.user;
+
+        //Log sign in date in DB
+        // 'update' method will only add last login info and won't overwrite everything else
+        //using 'set' would overwrite everything in the node
+        let logDate = new Date();
+        update(ref(db, 'users/' + user.uid + '/accountInfo'),{
+            last_login: logDate,
+        })
+        .then(()=>{
+            //User signed in successfully
+            alert("User signed in successfully :D")
+
+            //Get snapshot of all user info passed to login() and stored in session or local storage
+            //a snapshot - copy of a system's state at a specific point in time
+            //allows the "stay logged in" button
+            get(ref(db, 'users/' + user.uid + '/accountInfo')).then((snapshot)=>{
+                if(snapshot.exists()){
+                    console.log(snapshot.val());
+                    logIn(snapshot.val());
+                }else{
+                    console.log('User does not exist :(')
+                }
+            })
+            .catch((error)=>{
+                console.log(error);
+            });
+        })
+        .catch((error)=> {
+            alert(error)
+        });
+    })
+    .catch((error)=>{
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+    });
+}
